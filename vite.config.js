443afd5c9d3bdb0753a090dcd4746b9dcf6d89dd
Jitesh <jitugopale2426@gmail.com>
2025-12-24 11:04:02 +0530

    add preview in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'crud_frontend-main-621286e.kuberns.cloud'
+    ]
+  }
 })
